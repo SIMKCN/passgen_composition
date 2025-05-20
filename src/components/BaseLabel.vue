0170bb8b10e8528d2951a1a21f487fe5646b439b
SIMKCN <skmarschall@gmail.com>
2025-05-20 08:40:11 +0200

    BaseLabel

---
+++ src/components/BaseLabel.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const props = defineProps(['labelText'])
+</script>
+<template>
+    <p class="truncate bg-[#0077B6] text-[#CAF0F8] text-lg text-center shadow-2xl h-10 px-4 py-1">
+        {{ props.labelText }}
+    </p>
+</template>
