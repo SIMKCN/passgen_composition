f1d6c30c07d9011b098c4e90491ec773fd1546d9
SIMKCN <skmarschall@gmail.com>
2025-05-20 08:36:19 +0200

    basebutton component

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,3 +1,13 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps(['buttonLabel', 'iconSrc'])
 
 </script>
+<template>
+  <button class="shadow-2xl bg-[#00B4D8] rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 hover:bg-[#018eaa] text-lg px-1 py-1" @click="$emit('gotClicked')" >
+    {{ props.buttonLabel }}
+    <img v-if="props.iconSrc != 'noIcon'" :src="iconSrc" height="30" width="30">
+  </button>
+
+</template>
