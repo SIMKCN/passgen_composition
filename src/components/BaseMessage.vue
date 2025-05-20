2a9c668e6a177dd43bcd010ed93756382d13dfc0
SIMKCN <skmarschall@gmail.com>
2025-05-20 08:42:48 +0200

    BaseMessage Component

---
+++ src/components/BaseMessage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps(['labelMessage', 'textColor'])
+</script>
+<template>
+    <div>
+        <p class="font-semibold text-sm" :class="props.textColor">
+            {{ props.labelMessage }}
+        </p>
+    </div>
+
+</template>
