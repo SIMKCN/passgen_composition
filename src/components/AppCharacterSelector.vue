437920dd316bd342eab8971b50cacbb8bb0841ec
SIMKCN <skmarschall@gmail.com>
2025-05-20 10:41:24 +0200

    character selector

---
+++ src/components/AppCharacterSelector.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import BaseSwitchToggle from './BaseSwitchToggle.vue';
+
+interface Checkboxes {
+  label: string,
+  checked: boolean
+}
+
+const emit = defineEmits(['update_label_array', 'error_empty_label_array'])
+
+const checkboxesLabels = ref<Checkboxes[]>([
+        { label: "Großbuchstaben", checked: false },
+        { label: "Kleinbuchstaben", checked: false },
+        { label: "Symbole", checked: false },
+        { label: "Zahlen", checked: false },
+])
+
+
+function updateCheckboxArrayAndSendToApp($event) {
+  updateCheckboxArray($event)
+  const labels_checked_boxes = createArrayOfCheckedBoxes()
+  emitLabelsOrErrorToApp(labels_checked_boxes)
+}
+
+function updateCheckboxArray($event) {
+  for (let i = 0; i < checkboxesLabels.value.length; i++) {
+        if ($event == checkboxesLabels.value[i].label) {
+          checkboxesLabels.value[i].checked = !checkboxesLabels.value[i].checked;
+        }
+      }
+}
+
+function createArrayOfCheckedBoxes() {
+  const checked_boxes = checkboxesLabels.value.filter(
+    (item) => item.checked === true);
+      return checked_boxes.map((item) => item.label);
+}
+
+function emitLabelsOrErrorToApp(labels_checked_boxes: []) {
+  if (labels_checked_boxes.values.length != 0) {
+    emit("update_label_array", labels_checked_boxes)
+  } else {
+    emit('error_empty_label_array')
+  }
+}
+</script>
+<template>
+  <div
+    class="grid grid-cols-1 mt-5 mb-5 rounded-lg w-72 bg-[#0077B6] text-[#CAF0F8] px-2 py-2"
+  >
+    <BaseSwitchToggle
+      label="Großbuchstaben"
+      @got_checked="updateCheckboxArrayAndSendToApp($event)"
+    ></BaseSwitchToggle>
+    <BaseSwitchToggle
+      label="Kleinbuchstaben"
+      @got_checked="updateCheckboxArrayAndSendToApp($event)"
+    ></BaseSwitchToggle>
+    <BaseSwitchToggle
+      label="Symbole"
+      @got_checked="updateCheckboxArrayAndSendToApp($event)"
+    ></BaseSwitchToggle>
+    <BaseSwitchToggle
+      label="Zahlen"
+      @got_checked="updateCheckboxArrayAndSendToApp($event)"
+    ></BaseSwitchToggle>
+  </div>
+</template>
