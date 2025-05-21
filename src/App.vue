fb07d8f4e05ac6c992cb5abd3ea78ed3c9a30344
SIMKCN <skmarschall@gmail.com>
2025-05-21 08:44:26 +0200

    transfered variables to utility file

--- src/App.vue
+++ src/App.vue
@@ -7,118 +7,6 @@ import AppPasswordStrength from './components/AppPasswordStrength.vue';
 import AppCharacterSelector from './components/AppCharacterSelector.vue';
 import AppPasswordLengthInput from './components/AppPasswordLengthInput.vue';
 import { generatePassword } from './utilities/generatePassword.ts'
-const upperCaseLetters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-const lowerCaseLetters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-
-const numbers = [
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "0",
-];
-const symbols = [
-  "!",
-  "#",
-  "$",
-  "%",
-  "&",
-  "'",
-  "(",
-  ")",
-  "*",
-  "+",
-  ",",
-  "-",
-  ".",
-  "/",
-  ":",
-  ";",
-  "<",
-  "=",
-  ">",
-  "?",
-  "@",
-  "[",
-  "]",
-  "^",
-  "_",
-  "{",
-  "|",
-  "}",
-  "~",
-];
-const passphrase: string[] = []
-
 const isPasswordGenerated = computed(() => {
   return password.value.length > 0;
 })
@@ -244,7 +132,6 @@ function handlePasswordCopying() {
 function errorNoChars() {
   outputErrorCharacters()
   flipGenerateButtonStatus(false)
-  deletePassphrase()
 }
 function copyPasswordandOutputCopyMessage() {
   navigator.clipboard.writeText(password.value[0])
