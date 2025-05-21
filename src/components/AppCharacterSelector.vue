1ce003641744acc8d10990993280909cc9e3dc05
SIMKCN <skmarschall@gmail.com>
2025-05-21 09:18:25 +0200

    put interface next to Array

--- src/components/AppCharacterSelector.vue
+++ src/components/AppCharacterSelector.vue
@@ -2,12 +2,12 @@
 import { ref } from 'vue'
 import BaseSwitchToggle from './BaseSwitchToggle.vue';
 
+const emit = defineEmits(['update_label_array', 'error_empty_label_array'])
+
 interface Checkboxes {
   label: string,
   checked: boolean
 }
-
-const emit = defineEmits(['update_label_array', 'error_empty_label_array'])
 
 const checkboxesLabels = ref<Checkboxes[]>([
         { label: "Großbuchstaben", checked: false },
